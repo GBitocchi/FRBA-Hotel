4badbb8f42f7bfade841a68a27fda335bac0ec49
Lucas Romano <lromano@est.frba.utn.edu.ar>
2018-06-08 21:32:59 -0300

    ABM Hotel casi terminada, falta regimenes

--- AbmHotel/FechasMantenimiento.Designer.cs
+++ AbmHotel/FechasMantenimiento.Designer.cs
@@ -35,6 +35,9 @@
             this.label3 = new System.Windows.Forms.Label();
             this.btnAceptar = new System.Windows.Forms.Button();
             this.btnAtras = new System.Windows.Forms.Button();
+            this.txtDescripcion = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFechasIncorrectas = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dtInicio
@@ -83,27 +86,61 @@
             // 
             // btnAceptar
             // 
-            this.btnAceptar.Location = new System.Drawing.Point(108, 124);
+            this.btnAceptar.Location = new System.Drawing.Point(120, 244);
             this.btnAceptar.Name = "btnAceptar";
             this.btnAceptar.Size = new System.Drawing.Size(75, 23);
             this.btnAceptar.TabIndex = 5;
             this.btnAceptar.Text = "Aceptar";
             this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
             // btnAtras
             // 
-            this.btnAtras.Location = new System.Drawing.Point(198, 124);
+            this.btnAtras.Location = new System.Drawing.Point(201, 244);
             this.btnAtras.Name = "btnAtras";
             this.btnAtras.Size = new System.Drawing.Size(75, 23);
             this.btnAtras.TabIndex = 6;
             this.btnAtras.Text = "Atras";
             this.btnAtras.UseVisualStyleBackColor = true;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(32, 142);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(327, 96);
+            this.txtDescripcion.TabIndex = 7;
+            this.txtDescripcion.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Descripcion";
+            // 
+            // txtFechasIncorrectas
+            // 
+            this.txtFechasIncorrectas.AutoSize = true;
+            this.txtFechasIncorrectas.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFechasIncorrectas.ForeColor = System.Drawing.Color.Red;
+            this.txtFechasIncorrectas.Location = new System.Drawing.Point(376, 82);
+            this.txtFechasIncorrectas.Name = "txtFechasIncorrectas";
+            this.txtFechasIncorrectas.Size = new System.Drawing.Size(141, 18);
+            this.txtFechasIncorrectas.TabIndex = 9;
+            this.txtFechasIncorrectas.Text = "*Fechas incorrectas";
             // 
             // FechasMantenimiento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(388, 174);
+            this.ClientSize = new System.Drawing.Size(548, 293);
+            this.Controls.Add(this.txtFechasIncorrectas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDescripcion);
             this.Controls.Add(this.btnAtras);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.label3);
@@ -127,5 +164,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnAceptar;
         private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.RichTextBox txtDescripcion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label txtFechasIncorrectas;
     }
 }
--- AbmHotel/FechasMantenimiento.cs
+++ AbmHotel/FechasMantenimiento.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,20 +13,80 @@ namespace FrbaHotel.AbmHotel
 {
     public partial class FechasMantenimiento : Form
     {
-        string hotelID;
+        string hotelID, fechaInicio, fechaFin;
         public FechasMantenimiento(string idHotel)
         {
             InitializeComponent();
             hotelID = idHotel;
+            txtFechasIncorrectas.Visible = false;
+        }
+
+        private void ejecutarStoredProcedure()
+        {
+            SqlConnection db = DataBase.conectarBD();
+            SqlCommand bajaHotel = new SqlCommand("sp_BajaHotel", db);
+            bajaHotel.CommandType = CommandType.StoredProcedure;
+            bajaHotel.Parameters.AddWithValue("@id_hotel", hotelID);
+            bajaHotel.Parameters.AddWithValue("@fecha_inicio", dtInicio.Value);
+            bajaHotel.Parameters.AddWithValue("@fecha_fin", dtFin.Value);
+            bajaHotel.Parameters.AddWithValue("@descripcion", txtDescripcion.Text.Trim());
+            bajaHotel.ExecuteNonQuery();
+            db.Close();
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+
         }
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            DateTime fechaInicio = dtInicio.Value;
-            DateTime fechaFin = dtFin.Value;
-            string consultarPorReservas = string.Format("select * from CAIA_UNLIMITED.Reserva where hote_id = {0} and (rese_fecha_desde >= {1} and DATEADD(day, rese_cantidad_noches) <= {2}) and (DATEADD(day, rese_cantidad_noches) <= {2} and rese_fecha_desde >= {1})",
-                hotelID, fechaInicio.ToString("yyyy-mm-dd"), fechaFin.ToString("yyyy-mm-dd"));
+            fechaInicio = dtInicio.Value.ToString("yyyy-mm-dd");
+            fechaFin = dtFin.Value.ToString("yyyy-mm-dd");
+            if (dtFin.Value >= dtInicio.Value)
+            {
+                string consultarPorReservas = queryReservas();
+                if (DataBase.realizarConsulta(consultarPorReservas).Tables[0].Rows.Count == 0)
+                {
+                    string otrosMantenimientos = queryOtrosMantenimientos();
+                    if (DataBase.realizarConsulta(otrosMantenimientos).Tables[0].Rows.Count == 0)
+                    {
+                        ejecutarStoredProcedure();
+                        txtFechasIncorrectas.Visible = false;
+                        new HotelBajaExitosa().Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        new HayMantenimientos().Show();
+                        txtFechasIncorrectas.Visible = false;
+                    }
+                }
+                else
+                {
+                    new HayReservas().Show();
+                    txtFechasIncorrectas.Visible = false;
+                }
+            }
+            else
+            {
+                new CampoInvalido().Show();
+                txtFechasIncorrectas.Visible = true;
+            }
+        }
 
+        private string queryReservas()
+        {
+            string consultarPorReservas = string.Format("select * from CAIA_UNLIMITED.Reserva where hote_id = {0} and (DATEDIFF(day, rese_fecha_desde, '{1}') >= 0 and DATEDIFF(day, rese_fecha_desde, '{1}') <= DATEDIFF(day, rese_fecha_desde, DATEADD(day, rese_cantidad_noches, rese_fecha_desde))) or (DATEDIFF(day, rese_fecha_desde, '{2}') >= 0 and DATEDIFF(day, rese_fecha_desde, '{2}') <= DATEDIFF(day, rese_fecha_desde, DATEADD(day, rese_cantidad_noches, rese_fecha_desde)))",
+            hotelID, dtInicio.Value, dtFin.Value);
+            return consultarPorReservas;
+        }
+
+        private string queryOtrosMantenimientos()
+        {
+            string otrosMantenimientos = string.Format("select * from CAIA_UNLIMITED.Mantenimiento where hote_id = {0} and (DATEDIFF(day, mant_fecha_inicio, '{1}') >= 0 and DATEDIFF(day, mant_fecha_inicio, '{1}') <= DATEDIFF(day, mant_fecha_inicio, mant_fecha_fin)) or (DATEDIFF(day, mant_fecha_inicio, '{2}') >= 0 and DATEDIFF(day, mant_fecha_inicio, '{2}') <= DATEDIFF(day, mant_fecha_inicio, mant_fecha_fin))",
+                hotelID, dtInicio.Value, dtFin.Value);
+            return otrosMantenimientos;
         }
 
     }
