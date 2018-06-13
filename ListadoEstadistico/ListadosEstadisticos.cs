20f454bea2e3e7ed987ad7c2333409f62b667d70
Lucas Romano <lromano@est.frba.utn.edu.ar>
2018-06-13 06:39:59 -0700

    Subo primer vista de listado estadistico

---
+++ ListadoEstadistico/ListadosEstadisticos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaHotel.ListadoEstadistico
+{
+    public partial class ListadosEstadisticos : Form
+    {
+        public ListadosEstadisticos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMasCanceladas_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnConsumiblesFacturados_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnMasMantenimiento_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnMasOcupadas_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnClientesPuntos_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
