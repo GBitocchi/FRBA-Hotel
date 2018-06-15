7d35bd26b7b3d1b87c601d87be9987e83de9fdd9
Lucas Romano <lromano@est.frba.utn.edu.ar>
2018-06-15 13:59:34 -0300

    Subo algunos procedures y cosas en la vista del sistema

--- Menu Sistema/VistaSistema.Designer.cs
+++ Menu Sistema/VistaSistema.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnHotel = new System.Windows.Forms.Button();
+            this.btnHabitaciones = new System.Windows.Forms.Button();
+            this.btnFacturar = new System.Windows.Forms.Button();
+            this.btnListado = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnHotel
+            // 
+            this.btnHotel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHotel.Location = new System.Drawing.Point(55, 96);
+            this.btnHotel.Name = "btnHotel";
+            this.btnHotel.Size = new System.Drawing.Size(173, 28);
+            this.btnHotel.TabIndex = 0;
+            this.btnHotel.Text = "Menu hotel";
+            this.btnHotel.UseVisualStyleBackColor = true;
+            this.btnHotel.Click += new System.EventHandler(this.btnHotel_Click);
+            // 
+            // btnHabitaciones
+            // 
+            this.btnHabitaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHabitaciones.Location = new System.Drawing.Point(55, 130);
+            this.btnHabitaciones.Name = "btnHabitaciones";
+            this.btnHabitaciones.Size = new System.Drawing.Size(173, 28);
+            this.btnHabitaciones.TabIndex = 1;
+            this.btnHabitaciones.Text = "Menu habitaciones";
+            this.btnHabitaciones.UseVisualStyleBackColor = true;
+            // 
+            // btnFacturar
+            // 
+            this.btnFacturar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFacturar.Location = new System.Drawing.Point(55, 164);
+            this.btnFacturar.Name = "btnFacturar";
+            this.btnFacturar.Size = new System.Drawing.Size(173, 28);
+            this.btnFacturar.TabIndex = 2;
+            this.btnFacturar.Text = "Facturar estadia";
+            this.btnFacturar.UseVisualStyleBackColor = true;
+            // 
+            // btnListado
+            // 
+            this.btnListado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListado.Location = new System.Drawing.Point(55, 198);
+            this.btnListado.Name = "btnListado";
+            this.btnListado.Size = new System.Drawing.Size(173, 28);
+            this.btnListado.TabIndex = 3;
+            this.btnListado.Text = "Listado estadistico";
+            this.btnListado.UseVisualStyleBackColor = true;
             // 
             // VistaSistema
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 275);
+            this.Controls.Add(this.btnListado);
+            this.Controls.Add(this.btnFacturar);
+            this.Controls.Add(this.btnHabitaciones);
+            this.Controls.Add(this.btnHotel);
             this.Name = "VistaSistema";
             this.Text = "FRBA-HOTEL";
             this.ResumeLayout(false);
@@ -42,5 +91,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnHotel;
+        private System.Windows.Forms.Button btnHabitaciones;
+        private System.Windows.Forms.Button btnFacturar;
+        private System.Windows.Forms.Button btnListado;
     }
 }
--- Menu Sistema/VistaSistema.cs
+++ Menu Sistema/VistaSistema.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using FrbaHotel.AbmHotel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,28 @@ namespace FrbaHotel.Menu_Sistema
         public VistaSistema()
         {
             InitializeComponent();
+            chequearMantenimientos();
+        }
+
+        private static void chequearMantenimientos()
+        {
+            DataTable mantenimientos = DataBase.realizarConsulta("select * from CAIA_UNLIMITED.vw_MantenimientosTerminados").Tables[0];
+            foreach (DataRow mantenimiento in mantenimientos.Rows)
+            {
+                SqlConnection db = DataBase.conectarBD();
+                SqlCommand altaHotel = new SqlCommand("CAIA_UNLIMITED.sp_AltaHotel", db);
+                altaHotel.CommandType = CommandType.StoredProcedure;
+                altaHotel.Parameters.AddWithValue("@idHotel", mantenimiento[0]);
+                altaHotel.ExecuteNonQuery();
+                db.Close();
+            }
+        }
+
+        private void btnHotel_Click(object sender, EventArgs e)
+        {
+            //CHEQUEAR PERMISOS
+            new MenuHotel().Show();
+            this.Hide();
         }
     }
 }
