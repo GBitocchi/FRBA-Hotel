de1cc0a71e6f7d56b5c714c1435cf24ed5b31e4d
Lucas Romano <lromano@est.frba.utn.edu.ar>
2018-06-27 01:29:34 -0300

    Modifico crear huesped

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -791,9 +791,13 @@ GO
 CREATE PROCEDURE [CAIA_UNLIMITED].[sp_CrearHuesped] (@nombre nvarchar(255), @apellido nvarchar(255), @documento numeric(18,0), @tipo nvarchar(3), @mail nvarchar(255), @fecha_nacimiento datetime,@nacionalidad nvarchar(255),@calle nvarchar(255),@calle_nro numeric(18,0),@piso numeric(18,0),@dpto nvarchar(50),@ciudad nvarchar(255),@pais nvarchar(255), @telefono nvarchar(20))
 AS
 BEGIN
-	insert into CAIA_UNLIMITED.Direccion (dire_ciudad, dire_pais, dire_dom_calle, dire_nro_calle,
-											dire_telefono,dire_piso,dire_dpto)
-	values (@ciudad, @pais, @calle, @calle_nro, @telefono,@piso,@dpto)
+
+	if((SELECT COUNT(*) FROM CAIA_UNLIMITED.Direccion WHERE dire_telefono = @telefono AND dire_dom_calle = @calle AND dire_nro_calle = @calle_nro AND (dire_piso = @piso or @piso is null) AND dire_dpto = @dpto AND dire_ciudad = @ciudad  AND dire_pais = @pais ) = 0)
+        BEGIN
+			insert into CAIA_UNLIMITED.Direccion (dire_ciudad, dire_pais, dire_dom_calle, dire_nro_calle,dire_telefono,dire_piso,dire_dpto)
+			values (@ciudad, @pais, @calle, @calle_nro, @telefono,@piso,@dpto)
+        END
+	
 	insert into CAIA_UNLIMITED.Huesped (hues_mail,hues_nombre,hues_apellido,hues_documento,hues_nacionalidad,hues_nacimiento,hues_documento_tipo,hues_habilitado,dire_id)
 	values(@mail, @nombre,@apellido,@documento,@nacionalidad,convert(datetime,@fecha_nacimiento,120), @tipo,1,(SELECT dire_id FROM CAIA_UNLIMITED.Direccion WHERE dire_telefono = @telefono AND dire_dom_calle = @calle AND dire_nro_calle = @calle_nro AND (dire_piso = @piso or @piso is null) AND dire_dpto = @dpto AND dire_ciudad = @ciudad  AND dire_pais = @pais ))
 	
