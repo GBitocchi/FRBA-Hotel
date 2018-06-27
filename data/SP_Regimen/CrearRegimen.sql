e01327813732748edcfba1430f1eede17c3f7ead
Lucas Romano <lromano@est.frba.utn.edu.ar>
2018-06-27 03:03:11 -0300

    Borro carpetas

--- data/SP_Regimen/CrearRegimen.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE [dbo].[sp_CrearRegimen] (@codigo numeric(18,0),@descripcion nvarchar(255),@precio_base numeric(18,2),@estado bit)
-AS
-BEGIN
-	
-	insert into CAIA_UNLIMITED.Regimen (regi_codigo,regi_descripcion,regi_precio_base,regi_estado)
-	values(@codigo,@descripcion,@precio_base,@estado)
-					
-END
