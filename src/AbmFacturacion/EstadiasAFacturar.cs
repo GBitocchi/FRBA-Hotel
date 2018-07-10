d8fd8cf5cc19b3ab88e210149e00be6de04c9cc7
Lucas Romano <lromano@est.frba.utn.edu.ar>
2018-07-09 22:53:22 -0300

    Arreglo una query en facturacion

--- src/AbmFacturacion/EstadiasAFacturar.cs
+++ src/AbmFacturacion/EstadiasAFacturar.cs
@@ -16,7 +16,7 @@ namespace FrbaHotel.AbmFacturacion
         public EstadiasAFacturar(string idHotel)
         {
             InitializeComponent();
-            dgEstadias.DataSource = DataBase.realizarConsulta("select R.rese_codigo as 'Reserva', esta_codigo as 'Codigo', esta_fecha_inicio as 'Fecha de inicio', DATEDIFF(day, esta_fecha_inicio, esta_fecha_fin) as 'Cantidad de noches', habi_rese_id as 'Hotel' from CAIA_UNLIMITED.Estadia E join CAIA_UNLIMITED.Reserva R on (E.rese_codigo = R.rese_codigo) join CAIA_UNLIMITED.Habitacion_X_Reserva on (R.rese_codigo = habi_rese_codigo) where esta_fecha_fin is not null and habi_rese_id =" + idHotel + " order by esta_fecha_inicio").Tables[0];
+            dgEstadias.DataSource = DataBase.realizarConsulta("select R.rese_codigo as 'Reserva', esta_codigo as 'Codigo', esta_fecha_inicio as 'Fecha de inicio', DATEDIFF(day, esta_fecha_inicio, esta_fecha_fin) as 'Cantidad de noches', habi_rese_id as 'Hotel' from CAIA_UNLIMITED.Estadia E join CAIA_UNLIMITED.Reserva R on (E.rese_codigo = R.rese_codigo) join CAIA_UNLIMITED.Habitacion_X_Reserva on (R.rese_codigo = habi_rese_codigo) where esta_fecha_fin is not null and habi_rese_id =" + idHotel + " and esta_codigo in (select cons_esta_codigo_esta from CAIA_UNLIMITED.Consumible_X_Estadia) order by esta_fecha_inicio").Tables[0];
             dgEstadias.AllowUserToAddRows = false;
         }
 
